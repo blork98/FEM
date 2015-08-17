8dea15438f90382a6b0051f4506d637ceec5e150
blork98 <blork98@gmail.com>
2015-08-17 14:52:05 +0800

    Commit
    CGSolver

--- CGSolver.cpp
+++ CGSolver.cpp
@@ -3,6 +3,7 @@
 #include <LAOperations.h>
 #include <algorithm>
 #include <numeric>
+#include <limits>
 
 ControlParameters::ControlParameters(double tolerance)
 	:tolerance_(tolerance)
@@ -48,6 +49,9 @@ void CGSolver::apply_preconditioner(LAVector* rPrev) const
 
 void CGSolver::solve( LAVector& sol ) 
 {
+	if( A_->num_cols() != b_->size() )
+		throw;
+
 	//local vars
 	LAVector *rPrev, *rCurr, *pCurr, *pPrev, *solPrev, *solCurr;
 	rPrev = &r_prev;
@@ -101,3 +105,135 @@ void CGSolver::solve( LAVector& sol )
 
 };
 
+void CGSolver::set_A(const std::shared_ptr<LAMatrix>& A) 
+{
+	if (A_->num_cols() != b_->size() )
+	{
+		r_prev = std::move(LAVector(b_->container_type(),A_->num_cols()));
+		r_curr = std::move(LAVector(b_->container_type(),A_->num_cols()));
+		p_curr = std::move(LAVector(b_->container_type(),A_->num_cols()));
+		p_prev = std::move(LAVector(b_->container_type(),A_->num_cols()));
+		sol_prev = std::move(LAVector(b_->container_type(),A_->num_cols()));
+		q_curr = std::move(LAVector(b_->container_type(),A_->num_cols()));
+	};
+
+	A_ = A;
+};
+	
+void CGSolver::set_b(const std::shared_ptr<LAVector>& b)
+{
+
+	if( b->size() != b_->size() ) 
+	{
+		r_prev = std::move(LAVector(b->container_type(),b->size()));
+		r_curr = std::move(LAVector(b->container_type(),b->size()));
+		p_curr = std::move(LAVector(b->container_type(),b->size()));
+		p_prev = std::move(LAVector(b->container_type(),b->size()));
+		sol_prev = std::move(LAVector(b->container_type(),b->size()));
+		q_curr = std::move(LAVector(b->container_type(),b->size()));
+	};
+
+	b_ = b;
+};
+
+BCGSolver::BCGSolver( unsigned int maxIterations, std::shared_ptr<ControlParameters>& control,
+		const std::shared_ptr<LAMatrix>& A, const std::shared_ptr<LAVector>& b)
+		: maxIterations_(maxIterations), control_(control), A_(A), b_(b),
+		r_prev(b_->container_type(),b->size()), r_curr(b_->container_type(),b->size()),
+		rhat_prev(b_->container_type(),b->size()), rhat_curr(b_->container_type(),b->size()),
+		p_prev(b_->container_type(),b->size()), p_curr(b_->container_type(),b->size()),
+		phat_prev(b_->container_type(),b->size()), phat_curr(b_->container_type(),b->size()),
+		z_prev(b_->container_type(),b->size()), zhat_prev(b_->container_type(),b->size()),
+		q_curr(b_->container_type(),b->size()), qhat_curr(b_->container_type(),b->size()),
+		sol_prev(b_->container_type(),b->size()), usePreconditioner_(false),
+		alpha_curr(0.0), rho_prev(0.0), beta_prev(0.0), rho_prev2(0.0)
+{};
+
+void BCGSolver::apply_preconditioner(LAVector* rPrev, LAVector* rPrevHat) const
+{
+	//TODO
+	if(usePreconditioner_) {
+
+	} else {
+		z_prev = *rPrev;
+		zhat_prev = *rPrevHat;
+	};
+};
+
+void BCGSolver::solve( LAVector& sol )
+{
+	if( A_->num_cols() != b_->size() )
+		throw;
+
+	//local vars
+	LAVector *rPrev, *rCurr, *rHatPrev, *rHatCurr;
+	LAVector *pCurr, *pPrev, *pHatCurr, *pHatPrev;
+	LAVector *solPrev, *solCurr;
+
+	rPrev = &r_prev;
+	rCurr = &r_curr;
+	rHatPrev = &rhat_prev;
+	rHatCurr = &rhat_curr;
+	pCurr = &p_curr;
+	pPrev = &p_prev;
+	pHatPrev = &phat_prev;
+	pHatCurr = &phat_curr;
+
+	sol_prev = sol;
+	solPrev = &sol_prev;
+	solCurr = &sol;
+
+	//calculate initial residual
+	mat_vector_product(*A_,sol,*rPrev);
+	vector_subtract(*b_,*rPrev,*rPrev);
+
+	*rHatPrev = *rPrev;
+
+	//main iteration
+	for( unsigned int iter = 1; iter <= maxIterations_; ++iter )
+	{
+		//solve for z using precondition if enabled
+		apply_preconditioner(rPrev,rHatPrev);
+
+		rho_prev = vector_dot_product(*rHatPrev, z_prev);
+
+		if( std::abs(rho_prev - 0.0) < std::numeric_limits<double>::epsilon() )
+			throw;
+
+		if( iter != 1 ) {
+			beta_prev = rho_prev/rho_prev2;
+			vector_axpy(beta_prev, *pPrev, z_prev, *pCurr);	
+			vector_axpy(beta_prev,*pHatPrev, zhat_prev, *pHatCurr);
+		} else {
+			*pCurr = z_prev;
+			*pHatCurr = zhat_prev;
+		};
+
+		mat_vector_product(*A_,*pHatCurr,qhat_curr);
+		mat_vector_product(*A_,*pCurr,q_curr);
+
+		alpha_curr = rho_prev/vector_dot_product(*pHatCurr,q_curr);
+
+		vector_axpy(alpha_curr, *pCurr, *solPrev, *solCurr );
+		vector_xmay(alpha_curr, q_curr, *rPrev, *rCurr);
+		vector_xmay(alpha_curr, qhat_curr, *rHatPrev, *rHatCurr);
+
+		//check for convergence
+		if( control_->convergence_achieved(*rCurr) ) 
+		{
+			if( (iter % 2) == 0)
+				sol = sol_prev;
+
+			break;
+		};
+
+		//update prev variables
+		rho_prev2 = rho_prev;
+		std::swap(rPrev,rCurr);
+		std::swap(rHatPrev,rHatCurr);
+		std::swap(pPrev,pCurr);
+		std::swap(pHatPrev,pHatCurr);
+		std::swap(solPrev, solCurr);
+	};
+
+};
--- CGSolver.h
+++ CGSolver.h
@@ -19,6 +19,9 @@ public:
 		const std::shared_ptr<LAMatrix>& A, const std::shared_ptr<LAVector>& b);
 	void solve( LAVector& sol );
 	void apply_preconditioner(LAVector* rPrev) const;
+
+	void set_A(const std::shared_ptr<LAMatrix>& A);
+	void set_b(const std::shared_ptr<LAVector>& b);
 	
 private:
 	std::shared_ptr<ControlParameters> control_;
@@ -27,6 +30,25 @@ private:
 	unsigned int maxIterations_;
 	mutable LAVector r_prev, r_curr, z_prev, z_curr, p_curr, p_prev, q_curr, sol_prev;
 	mutable double alpha_prev, alpha_curr, rho_curr, rho_prev, rho_prev2, beta_prev;
+	bool usePreconditioner_;
+};
+
+class BCGSolver : public LinearSolver
+{
+public:
+	BCGSolver( unsigned int maxIterations, std::shared_ptr<ControlParameters>& control,
+		const std::shared_ptr<LAMatrix>& A, const std::shared_ptr<LAVector>& b);
+	void solve( LAVector& sol );
+	void apply_preconditioner(LAVector* rPrev, LAVector* rPrevHat) const;
+private:
+	std::shared_ptr<ControlParameters> control_;
+	std::shared_ptr<LAMatrix> A_;
+	std::shared_ptr<LAVector> b_;
+	unsigned int maxIterations_;
+	mutable LAVector r_prev, rhat_prev, r_curr, rhat_curr;
+	mutable LAVector z_prev, zhat_prev, sol_prev, q_curr, qhat_curr;
+	mutable LAVector p_curr, phat_curr, p_prev, phat_prev;
+	mutable double alpha_curr, rho_prev, rho_prev2, beta_prev;
 	bool usePreconditioner_;
 };
 
